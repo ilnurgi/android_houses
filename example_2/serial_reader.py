7411aa05ed9eeabb8eb1f85eddff233eacbdc9f1
ilnurgi <ilnurgi87@gmail.com>
2016-01-18 15:32:45 +0300

    u ex_2

--- example_2/serial_reader.py
+++ example_2/serial_reader.py
@@ -5,12 +5,29 @@
 """
 
 import sys
+import time
+
 import serial
 
 arduino_tty = '/dev/ttyACM0'
 speed = 9600
 
-ser = serial.Serial(arduino_tty, speed)
-print 'connect to {0}, speed={1}\nwhite data\n'
+try:
+    ser = serial.Serial(arduino_tty, speed)
+except serial.serialutil.SerialException:
+    print 'device {0}, not found'.format(arduino_tty)
+    exit(0)
+else:
+    print 'connect to {0}, speed={1}\nwhite data\n'.format(arduino_tty, speed)
+
+
 while True:
-    print ser.readline()
+    try:
+        print ser.readline()
+        counter = 1
+    except serial.serialutil.SerialException:
+        print '{0}/10 error get data'.format(counter)
+        time.sleep(5)
+        counter += 1
+        if counter > 10:
+            break
